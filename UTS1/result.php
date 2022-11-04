8f6d077f40ddb72488ecb186ccdd503e901dd1ce
Adelia Zhafirah <adeliaz2704@gmail.com>
2022-11-04 11:01:19 +0700

    first commit

---
+++ UTS1/result.php
@@ -0,0 +1,11 @@
+<?php
+$first = $_POST['fname'];
+$last = $_POST['lname'];
+$email = $_POST['email'];
+$select1 = $_POST['fav_language'];
+$phone = $_POST['phone'];
+$birth = $_POST['birthday'];
+$message = $_POST['msg'];
+
+echo "First Name : $first <br> Last Name : $last <br> email : $email <br> select : $select1 <br> Phone number : $phone <br> Birth : $birth <br> Message : $message"
+?>
